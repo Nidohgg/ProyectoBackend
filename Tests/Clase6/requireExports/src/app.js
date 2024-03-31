afdf928d3188bf49b1980fabb1e8bac874ee5d9d
Emanuel Torres <emauelema562@gmail.com>
2024-03-31 19:48:46 -0300

    Clase 6

--- Tests/Clase6/requireExports/src/app.js
+++ Tests/Clase6/requireExports/src/app.js
@@ -2,7 +2,7 @@ const express=require("express")
 const path=require("path")
 const UserManager=require("./clases/userManager")
 
-const PORT=3000;
+const PORT=3050;
 const app=express();
 
 const userManager=new UserManager(path.resolve(__dirname, './data/usuarios.json'))
